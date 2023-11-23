5aabf26886c08528e37091ccdc204d9aa461b978
Davs <123772756+Davstx@users.noreply.github.com>
2023-11-23 15:59:57 -0300

    docs: inicializando o projeto da API de consulta

---
+++ consultaApi/src/main/java/com/github/consultaApi/ConsultaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.consultaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaApiApplication.class, args);
+	}
+
+}
