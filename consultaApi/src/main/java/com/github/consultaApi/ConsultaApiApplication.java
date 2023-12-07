2196498b77e96ffabcdef1d00d81d47704dca86e
Davstx <davidsilvateixeira5@hotmail.com>
2023-12-07 15:39:24 -0300

    refactor: método getGameById agora puxa a rota /games/:game_id

--- consultaApi/src/main/java/com/github/consultaApi/ConsultaApiApplication.java
+++ consultaApi/src/main/java/com/github/consultaApi/ConsultaApiApplication.java
@@ -1,12 +1,10 @@
 package com.github.consultaApi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-
 
 @SpringBootApplication
 public class ConsultaApiApplication {
